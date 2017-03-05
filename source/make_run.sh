6f64d141318a7db640b6d22ea5596da2928e57c9
evandrocoan <evandrocoan@hotmail.com>
2017-03-05 00:49:01 -0300

    Moved the program run from the make_run.sh into the Makefile

--- source/make_run.sh
+++ source/make_run.sh
@@ -7,18 +7,15 @@ if [[ $FIRST_COMMAND_ARGUMENT == "main" ]]
 then
     make clean
     make
-    ./main argument1 argument2
 
 elif [[ $FIRST_COMMAND_ARGUMENT == "veryclean" ]]
 then
     make $FIRST_COMMAND_ARGUMENT
     make
-    ./main
 
 else
     make clean
     make $FIRST_COMMAND_ARGUMENT
-    ./main
 fi
 
 
