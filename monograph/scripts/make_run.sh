8014584ebcff394eca7d8f66762f9ddf6b9f7758
evandrocoan <evandrocoan@hotmail.com>
2017-04-28 14:39:47 -0300

    Fixed the monograph/scripts/make_run.sh not importing correctly
    when ran from another directory.

--- monograph/scripts/make_run.sh
+++ monograph/scripts/make_run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Reliable way for a bash script to get the full path to itself?
+# http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+pushd `dirname $0` > /dev/null
+SCRIPT_FOLDER_PATH=`pwd`
+popd > /dev/null
+
 # Import the helper functions.
-. ./timer_calculator.sh
+. $SCRIPT_FOLDER_PATH/timer_calculator.sh
 
 
 cd $1
