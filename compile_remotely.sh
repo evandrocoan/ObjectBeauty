3c55746d5594192a2aaa8014e776989ebe4df460
evandrocoan <evandrocoan@hotmail.com>
2018-09-07 20:09:34 -0300

    Cannot sync changes back form the network drive because they mess
    up everything

--- compile_remotely.sh
+++ compile_remotely.sh
@@ -7,7 +7,10 @@ popd > /dev/null
 printf 'Synchronazing local files...\n'
 printf "%s\n\n" "$SCRIPT_FOLDER_PATH"
 
-unison -ignore "Name ObjectBeautifier.sublime-workspace" -ignore "Name .git" \
+unison -ignore "Name ObjectBeautifier.sublime-workspace" \
 $SCRIPT_FOLDER_PATH/ /cygdrive/l/Arquivos/ObjectBeautifier -auto
+
+# Cannot sync changes back form the network drive because they mess up everything
+# rsync -rvu /cygdrive/l/Arquivos/ObjectBeautifier/ $SCRIPT_FOLDER_PATH --exclude={ObjectBeautifier.sublime-workspace} --delete
 
 
