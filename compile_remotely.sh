b892233b8b5f96029ec645606bcd38c08642346f
evandrocoan <evandrocoan@hotmail.com>
2018-09-07 19:07:18 -0300

    Added -ignore "Name .git" to compile_remotely.sh

--- compile_remotely.sh
+++ compile_remotely.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 printf 'Synchronazing local files...\n'
 printf "%s\n\n" "$SCRIPT_FOLDER_PATH"
 
-unison -ignore "Name ObjectBeautifier.sublime-workspace" \
+unison -ignore "Name ObjectBeautifier.sublime-workspace" -ignore "Name .git" \
 $SCRIPT_FOLDER_PATH/ /cygdrive/l/Arquivos/ObjectBeautifier -auto
 
 
