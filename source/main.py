c5b10bfe455556e7416814813fc9d6bd3bc3dcab
evandrocoan <evandrocoan@hotmail.com>
2018-10-01 22:25:21 -0300

    Implemented the first version for the source/main.py semantic
    analyzer.

--- source/main.py
+++ source/main.py
@@ -6,8 +6,14 @@ import semantic_analyzer
 from lark import Lark
 from lark.indenter import Indenter
 
+from pprint import pformat
+from debug_tools import getLogger
+
+log = getLogger(__name__)
+
+
 def make_png(lark_tree, output_file):
-    lark.tree.pydot__tree_to_png( lark_tree, output_file)
+    lark.tree.pydot__tree_to_png( lark_tree, output_file, "TB")
 
 def get_relative_path(relative_path, script_file):
     """
@@ -23,38 +29,24 @@ grammar_file_path = get_relative_path( "gramatica_compiladores.lark", __file__ )
 
 ## The parser used to build the Abstract Syntax Tree and parse the input text
 with open( grammar_file_path, "r", encoding='utf-8' ) as file:
-    meu_parser = Lark( file.read(), start='language_syntax', parser='lalr', lexer='contextual', transformer=semantic_analyzer.TreeTransformer )
+    meu_parser = Lark( file.read(), start='language_syntax', parser='lalr', lexer='contextual')
 
-simples_exemplo = """
-name: Abstract Machine Language
-scope: source.sma
-contexts:
-    match: (true|false)
-        scope: constant.language
-"""
-
-"""
-apt-cyg update
-awk: cmd. line:4: fatal: cannot open file `/etc/setup/setup.rc' for reading (No such file or directory)
-awk: fatal: cannot open file `/etc/setup/setup.rc' for reading (No such file or directory)
-mkdir: cannot create directory ‘//x86’: Read-only file system
-/usr/bin/apt-cyg: line 158: cd: //x86: No such file or directory
-/x86/setup.bz2: Scheme missing.
-Error updating setup.ini, reverting
-"""
 
 # To generate the lexer/parser
 # python3 -m lark.tools.standalone /cygdrive/l/Arquivos/gramatica_compiladores.lark > lexer.py
 def test():
-    # tree = meu_parser.parse(simples_exemplo)
-    # print(tree.pretty())
     # grammar_file_path = get_relative_path( "exemplos/programa_exemplo.beauty-grammar", __file__ )
     grammar_file_path = get_relative_path( "exemplos/duplicated_contexts.beauty-grammar", __file__ )
 
     with open( grammar_file_path, "r", encoding='utf-8' ) as file:
         tree = meu_parser.parse(file.read())
-        # print(tree.pretty())
-        make_png( tree, get_relative_path( "exemplos/duplicated_contexts.png", __file__ ) )
+        # make_png( tree, get_relative_path( "exemplos/duplicated_contexts.png", __file__ ) )
+        # log( 1, tree.pretty() )
+
+        new_tree = semantic_analyzer.TreeTransformer().transform( tree )
+        # make_png( tree, get_relative_path( "exemplos/duplicated_contexts.png", __file__ ) )
+        log( 1, "\n%s", new_tree.pretty() )
+
 
 if __name__ == '__main__':
     test()
--- source/semantic_analyzer.py
+++ source/semantic_analyzer.py
@@ -1,17 +1,78 @@
 
 import lark
-from lark import Tree
+from lark import Tree, LarkError, Token
+
+from debug_tools import getLogger
+log = getLogger(__name__)
+
+
+class SemanticErrors(LarkError):
+    def __init__(self, exceptions):
+        counter = [0]
+
+        def increment(exception):
+            counter[0] += 1
+            return str(exception)
+
+        self.messages = "\n1. " + ( "\n%s. " % counter[0] ).join( increment(exception) for exception in exceptions )
+
+    def __str__(self):
+        return self.messages
+
 
 class TreeTransformer(lark.Transformer):
     """
-        Transforms the AST (Abstract Syntax Tree) nodes into meaningful string representations,
-        allowing simple recursive parsing parsing of the AST tree.
+        Transforms the Derivation Tree nodes into meaningful string representations,
+        allowing simple recursive parsing parsing and conversion to Abstract Syntax Tree.
     """
 
-    def start_symbol(self, productions):
-        """
-            Converts the tree start symbol into a production ready to be used in the Chomsky Grammar.
-        """
-        log( 4, 'productions: %s', productions )
-        return productions[0]
+    ## Saves all the semantic errors errors detected so far
+    errors = []
 
+    ## Can only be one scope called `contexts`
+    has_called_language_construct_rules = False
+
+    def language_syntax(self, tree):
+        """
+            This is the grammar start symbol and will be called by last with all
+            partial subtrees the start symbol derivates.
+
+            This is a great place to check whether global grammar properties
+            where set.
+        """
+
+        if not self.has_called_language_construct_rules:
+            self.errors.append("You must to define the `contexts` block in your grammar!")
+
+        if self.errors:
+            raise SemanticErrors(self.errors)
+
+        return self.__default__(tree.data, tree.children, tree.meta)
+
+    def language_construct_rules(self, tree):
+        self.has_called_language_construct_rules = True
+        return self.__default__(tree.data, tree.children, tree.meta)
+
+    def miscellaneous_language_rules(self, tree):
+        first_token = tree.children[0]
+        assert tree.data == 'miscellaneous_language_rules', "Just documenting what the data attribute has."
+        assert isinstance( first_token, Token ), "The first children must be a Token, while the second is a subtree."
+
+        if str(first_token) == 'contexts':
+            self.errors.append("Extra `contexts` rule defined in your grammar on: %s" % first_token.pretty())
+
+        return self.__default__(tree.data, tree.children, tree.meta)
+
+    def _call_userfunc(self, tree, new_children=None):
+        """
+            Overrides the default behavior of TreeTransformer, to send the whole
+            three to its children instead of only trees children.
+        """
+        # Assumes tree is already transformed
+        children = tree.children if new_children is None else new_children
+        try:
+            f = getattr(self, tree.data)
+        except AttributeError:
+            return self.__default__(tree.data, tree.children, tree.meta)
+        else:
+            return f(tree)
